283504da295c97b175be49d68c728fa65e010ceb
pratyush <82261854+helloiampratyush@users.noreply.github.com>
2024-04-17 19:41:44 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,10 @@
 import { ConnectButton } from "web3uikit";
-import Link from "next/link";
 import { useState, useEffect } from "react";
+import Link from "next/link";
 
 export default function Header() {
     const [prevScrollPos, setPrevScrollPos] = useState(0);
     const [visible, setVisible] = useState(true);
-    const [menuOpen, setMenuOpen] = useState(false); // State to track if the mobile menu is open
 
     useEffect(() => {
         const handleScroll = () => {
@@ -21,35 +20,22 @@ export default function Header() {
         return () => window.removeEventListener("scroll", handleScroll);
     }, [prevScrollPos, visible]);
 
-    // Function to toggle mobile menu
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    };
-
     return (
         <>
             <nav
-                className={`items-center p-2 bg-yellow-500 border-b-2 flex flex-row fixed top-0 w-full z-10 transition-all ${
+                className={`p-2 bg-yellow-500 border-b-2 flex fixed top-0 w-full z-10 transition-all ${
                     visible ? "" : "-translate-y-full"
                 }`}
             >
                 <h1 className="py-4 px-4 font-bold text-3xl">Lottery</h1>
-                <button className="mr-4 p-6 mx-9 hover:text-slate-100 focus:ring focus:ring-violet-300 hover:bg-sky-700 active:bg-violet-700" onClick={toggleMenu}>MENU</button>
+                <Link href="/">
+                    <button className="mr-4 p-6 mx-9 hover:text-slate-100 focus:ring focus:ring-violet-300 hover:bg-sky-700 active:bg-violet-700">HOME</button>
+                </Link>
                 <div className="flex flex-grow mr-4 justify-end">
                     <ConnectButton moralisAuth={false} />
                 </div>
             </nav>
-            {/* Collapsible mobile menu */}
-            <div className={`fixed top-0 left-0 h-full w-full bg-yellow-500 transition-all ${menuOpen ? "visible" : "invisible"}`}>
-                <div className="flex flex-col items-center justify-center h-full">
-                    <Link href="/">
-                        <button className="my-4 p-6 mx-9 hover:text-slate-100 focus:ring focus:ring-violet-300 hover:bg-sky-700 active:bg-violet-700" onClick={toggleMenu}>HOME</button>
-                    </Link>
-                    {/* Add more menu items as needed */}
-                </div>
-            </div>
             <div className="h-20"></div> {/* Placeholder to prevent content from being obscured by fixed navbar */}
         </>
     );
 }
-
